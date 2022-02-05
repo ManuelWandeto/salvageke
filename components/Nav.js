a64042930303c41c24bd3778393d40ff4a0b3d4b
ManuelWandeto <immanuelmojo@hotmail.com>
2022-02-05 11:20:53 +0300

    include selectHandler prop & configure routing with next/link

--- components/Nav.js
+++ components/Nav.js
@@ -6,23 +6,32 @@ import { GoGear } from "react-icons/go";
 import { HiInformationCircle } from "react-icons/hi";
 import { FaPhone } from "react-icons/fa";
 import styles from "../styles/nav.module.css";
-// import { Nav } from "react-bootstrap";
+import Link from "next/link";
 
-const Navigation = () => {
+const Navigation = ({ selectHandler }) => {
   return (
     <ul
       className={`navbar-nav ${styles.nav} justify-content-around`}
     >
       <li>
-        <a href="#" className={`${styles.nav_link} active`}>
-          <span className={`${styles.icon} d-md-none`}>
-            <FaHome />
-          </span>
-          <span className="label">Home</span>
-        </a>
+        <Link href="/">
+          <a
+            className={`${styles.nav_link} active`}
+            onClick={selectHandler}
+          >
+            <span className={`${styles.icon} d-md-none`}>
+              <FaHome />
+            </span>
+            <span className="label">Home</span>
+          </a>
+        </Link>
       </li>
       <li>
-        <a href="#" className={styles.nav_link}>
+        <a
+          href="#"
+          className={styles.nav_link}
+          onClick={selectHandler}
+        >
           <span className={`${styles.icon} d-md-none`}>
             <FaCar />
           </span>
@@ -30,15 +39,24 @@ const Navigation = () => {
         </a>
       </li>
       <li>
-        <a href="#" className={styles.nav_link}>
-          <span className={`${styles.icon} d-md-none`}>
-            <FaGavel />
-          </span>
-          <span className="label">Auction</span>
-        </a>
+        <Link href="/auctions">
+          <a
+            className={styles.nav_link}
+            onClick={selectHandler}
+          >
+            <span className={`${styles.icon} d-md-none`}>
+              <FaGavel />
+            </span>
+            <span className="label">Auction</span>
+          </a>
+        </Link>
       </li>
       <li>
-        <a href="#" className={styles.nav_link}>
+        <a
+          href="#"
+          className={styles.nav_link}
+          onClick={selectHandler}
+        >
           <span className={`${styles.icon} d-md-none`}>
             <GiSpanner />
           </span>
@@ -46,7 +64,11 @@ const Navigation = () => {
         </a>
       </li>
       <li>
-        <a href="#" className={styles.nav_link}>
+        <a
+          href="#"
+          className={styles.nav_link}
+          onClick={selectHandler}
+        >
           <span className={`${styles.icon} d-md-none`}>
             <GoGear />
           </span>
@@ -54,7 +76,11 @@ const Navigation = () => {
         </a>
       </li>
       <li>
-        <a href="#" className={styles.nav_link}>
+        <a
+          href="#"
+          className={styles.nav_link}
+          onClick={selectHandler}
+        >
           <span className={`${styles.icon} d-md-none`}>
             <HiInformationCircle />
           </span>
@@ -62,7 +88,11 @@ const Navigation = () => {
         </a>
       </li>
       <li>
-        <a href="#" className={styles.nav_link}>
+        <a
+          href="#"
+          className={styles.nav_link}
+          onClick={selectHandler}
+        >
           <span className={`${styles.icon} d-md-none`}>
             <FaPhone />
           </span>
