42923b845ecdb7630d49c097a282e920b7ceea73
ManuelWandeto <immanuelmojo@hotmail.com>
2022-02-05 18:36:42 +0300

    adapted index.js for moved styles

--- pages/index.js
+++ pages/index.js
@@ -2,18 +2,15 @@ import Header from "../components/Header";
 import AuctionCarousel from "../components/Carousel";
 import AuctionForm from "../components/AuctionForm.js";
 import { Container } from "react-bootstrap";
-import styles from "../styles/index.module.css";
 
 const Home = () => {
   return (
     <>
-      <section className={styles.content}>
+      <section className="content">
         <Container fluid="md">
           <div className="col-sm-12">
             <div className="row">
-              <h1 className={styles.h1}>
-                Welcome to Salvage Kenya
-              </h1>
+              <h1>Welcome to Salvage Kenya</h1>
               <p>
                 Lorem ipsum dolor, sit amet consectetur
                 adipisicing elit. Officiis natus magni,
@@ -25,10 +22,10 @@ const Home = () => {
             </div>
           </div>
           <div className="block mb-4">
-            <div className={styles.home_block_header}>
+            <div style={{ marginTop: "3.5px" }}>
               <h2>Sell your Car</h2>
               <p>
-                <strong className={styles.strong}>
+                <strong>
                   We buy any vehicle in any condition with a
                   fixed price guarantee
                 </strong>
@@ -37,10 +34,10 @@ const Home = () => {
             <AuctionForm />
           </div>
           <div className="block">
-            <div className={styles.home_block_header}>
+            <div style={{ marginTop: "3.5px" }}>
               <h2>Online Auction</h2>
               <p>
-                <strong className={styles.strong}>
+                <strong>
                   Featured vehicles, car parts and all that
                   jazz
                 </strong>
