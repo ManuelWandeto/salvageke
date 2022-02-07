d2a180c35a1e4561344a3b56fa1daacc46aeef36
ManuelWandeto <immanuelmojo@hotmail.com>
2022-02-07 23:09:17 +0300

    add domain for wrecked car images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["images.hgmsites.net"],
+    domains: [
+      "images.hgmsites.net, asmeu1.s3.amazonaws.com",
+    ],
   },
 };
