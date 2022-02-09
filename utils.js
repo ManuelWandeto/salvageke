4396e3a2486318138e2d91dddcc19f059e9c9137
ManuelWandeto <immanuelmojo@hotmail.com>
2022-02-09 17:40:02 +0300

    implement function to get select options from data

--- utils.js
+++ utils.js
@@ -21,3 +21,15 @@ export const paginate = (dataArr, pageSize) => {
 export function formatPrice(x) {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
+
+export const getSelectOptions = (dataArr, keys) => {
+  const options = {};
+  for (let i = 0; i < keys.length; i++) {
+    const set = new Set();
+    dataArr.forEach((item) => {
+      set.add(item[keys[i]]);
+    });
+    options[keys[i]] = [...set];
+  }
+  return options;
+};
