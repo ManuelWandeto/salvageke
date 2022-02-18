4fab70d478a464df219da971f2c0bc77252d2a3d
ManuelWandeto <immanuelmojo@hotmail.com>
2022-02-19 01:05:23 +0300

    move paginate functionality from client to server

--- utils.js
+++ utils.js
@@ -1,23 +1,3 @@
-export const paginate = (dataArr, pageSize) => {
-  let startNumber = 0;
-  let endNumber = pageSize;
-  let pages = [];
-  let totalPages = Math.ceil(dataArr.length / pageSize);
-  for (let i = 0; i < totalPages; i++) {
-    pages.push({
-      pageNumber: i + 1,
-      items: dataArr.slice(startNumber, endNumber),
-    });
-    startNumber = endNumber;
-    endNumber += pageSize;
-  }
-  return {
-    totalPages,
-    pages,
-    totalItems: dataArr.length,
-  };
-};
-
 export function formatPrice(x) {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
