7b55522c87d1396de0b70d1558f29bb424c968b4
ManuelWandeto <immanuelmojo@hotmail.com>
2022-02-19 14:24:23 +0300

    replace paginate function for carousel

--- utils.js
+++ utils.js
@@ -1,3 +1,22 @@
+export const paginate = (dataArr, pageSize) => {
+  let startNumber = 0;
+  let endNumber = pageSize;
+  let pages = [];
+  let totalPages = Math.ceil(dataArr.length / pageSize);
+  for (let i = 0; i < totalPages; i++) {
+    pages.push({
+      pageNumber: i + 1,
+      items: dataArr.slice(startNumber, endNumber),
+    });
+    startNumber = endNumber;
+    endNumber += pageSize;
+  }
+  return {
+    totalPages,
+    pages,
+  };
+};
+
 export function formatPrice(x) {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
