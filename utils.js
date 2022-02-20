e83af4c84fc37c01373395eab7476b58e331ed76
ManuelWandeto <immanuelmojo@hotmail.com>
2022-02-20 09:06:53 +0300

    refactor getSelectOptions to take one parameter

--- utils.js
+++ utils.js
@@ -21,7 +21,8 @@ export function formatPrice(x) {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
-export const getSelectOptions = (dataArr, keys) => {
+export const getSelectOptions = (dataArr) => {
+  let keys = Object.keys(dataArr[0]);
   const options = {};
   for (let i = 0; i < keys.length; i++) {
     const set = new Set();
